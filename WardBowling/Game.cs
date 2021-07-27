cbeb4f404d049937a25517b9000a7e9862e634e9
Ward-dev <ward_nijs@hotmail.com>
2021-07-27 15:17:20 +0200

    Added game class, check for strikes, check for spares and throws

---
+++ WardBowling/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WardBowling
+{
+    class Game
+    {
+        private int[] frame = new int[10];
+        private int[] throws = new int[20];
+        int currentThrow = 0;
+
+        public bool Strike(int throwNumber)
+        {
+            return throws[throwNumber] == 10;
+        }
+
+        public bool Spare(int throwNumber)
+        {
+            return throws[throwNumber] + throws[throwNumber + 1] == 10;
+        }
+
+        public void Throw(int amountOfPins) 
+        {
+            throws[currentThrow] = amountOfPins;
+        }
+
+
+    }
+}
